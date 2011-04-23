e2281a5730c3666d98f2c1497adae2b2b2544aec
Jesse Franceschini <j.franceschini@gmail.com>
2011-04-22 21:30:42 -0700

    Changed database settings, added local settings to ignore.

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,24 +1,16 @@
 # Django settings for myproject project.
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Jesse Franceschini', 'j.franceschini@gmail.com'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -143,3 +135,5 @@ LOGGING = {
         },
     }
 }
+
+from settings_local import *
