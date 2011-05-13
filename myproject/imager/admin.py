bb8e7942610c15d62fe4f1712bebbcf7cca01055
Jesse Franceschini <j.franceschini@gmail.com>
2011-05-12 23:35:58 -0700

    Imager admin changes.

--- myproject/imager/admin.py
+++ myproject/imager/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from myproject.imager.models import Page
+from myproject.imager.models import Page, Image
+
+class ImageAdmin(admin.ModelAdmin):
+	pass
 
 class PageAdmin(admin.ModelAdmin):
-	list_display = ('admin_thumbnail', 'title',)
+	list_display = ('filename', 'title', 'admin_thumbnail',)
+	# inlines = [ImageInline,]
 
+admin.site.register(Image, ImageAdmin)
 admin.site.register(Page, PageAdmin)
